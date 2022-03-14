1ce115be8317ba831096cccacb16a7b57c22dffc
rohitravirane <rohitravikantrane@gmail.com>
2022-03-14 10:32:09 +0530

    project push

---
+++ admin/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tour1";
+
+$currency = "INR";
+
+
+date_default_timezone_set('Asia/Kolkata');
+
+
+
+
+
+$smtp_host = 'smtp.gmail.com';
+$smtp_user = 'yourmail@gmail.com';
+$smtp_pass = 'EJWGDSDSIYIQNHVB';
